041893f61431e783583de02d8d3caaa1b9d5848b
Abiy Seifu <abiy@ripple.com>
2014-04-10 12:59:58 -0700

    [FEATURE] blockchain call test

--- test/blockchain.js
+++ test/blockchain.js
@@ -1,23 +1,24 @@
 var assert = require('assert');
-var blockchain = require('../lib/incoming_transaction_processor.js');
+var blockchain = require('../lib/blockchain_client.js');
 var config = require('../config/nconf');
 var supertest = require('supertest');
 
-describe('Ping Blockchain.info API', function(){
+describe('Test Blockchain.info', function(){
+    var timeout = 20000;
     describe('\nping', function(){
         it('should ping blockchain api', function(done){
+            this.timeout(timeout);
             blockchain.ping(function(err, resp, body){
-                assert(resp.statusCode === 200);
+                assert.strictEqual(resp.statusCode, 200);
                 done();
             });
         });
     });
-});
 
-describe('Get newly received payment from Blockchain.info', function(){
-   describe('\nendpint call', function(){
+   describe('\nendpoint call: get newly received payment', function(){
+       this.timeout(timeout);
         it('should return a number', function(done){
-            blockchain.getReceived(config.get('BTC_INBOUND'), function(err, body){
+            blockchain.getReceived(config.get('BTC_INBOUND'), function(err, resp, body){
                 assert(Number(body) != NaN);
                 done();
             });
