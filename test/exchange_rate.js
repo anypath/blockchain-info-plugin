f0520f96cc447e26f8a8304dcc0ccc8ff3e88bf8
Abiy Seifu <abiy@ripple.com>
2014-04-10 13:00:41 -0700

    [FEATURE] exchange rate call unit test

---
+++ test/exchange_rate.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var config = require('../config/nconf');
+var exchangeRate = require('../lib/convert.js');
+
+describe('Call ripple charts API', function(){
+    it('should return a response code of 200', function(done){
+        exchangeRate.getRate(function(err, resp, body){
+            assert.strictEqual(resp.statusCode, 200);
+            assert(body.length >= 1);
+            assert(body[0].hasOwnProperty('base'));
+            assert.equal(body[0]['base']['currency'], 'BTC');
+            assert.equal(body[0]['trade']['currency'], 'XRP');
+            done();
+        });
+    });
+});
+
