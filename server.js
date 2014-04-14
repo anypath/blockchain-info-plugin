fcdd53c85b239578c21d1455686003e35a951c0a
Abiy Seifu <abiy@ripple.com>
2014-04-13 20:50:27 -0700

    [CHORE] code clean up

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var config = require('./config/nconf'),
-    gateway = require('ripple-gateway'),
     blockchain = require('./lib/blockchain_listener'),
     depositProcessor = require('./lib/deposit_processor'),
-    outgoing = require('./lib/outgoing_processor');
-
     hotWallet = require('./lib/poll_hot_wallet_balance');
 
 blockchain.listen(function(btc){
 
     depositProcessor.start(btc, function(transaction){
-        gateway.deposits.record(transaction, console.log);
+//        gateway.deposits.record(transaction, function(transaction){
+//            console.log(transaction);
+//        });
+
     });
 
 });
