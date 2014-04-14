da3047ef37539435eaa3166b97a6ad3f6c0148c5
Abiy Seifu <abiy@ripple.com>
2014-04-13 20:49:51 -0700

    [FEATURE] mailer test

---
+++ test/mailer.js
@@ -0,0 +1,11 @@
+var config = require('../config/nconf'),
+    assert = require('assert'),
+    mailer = require('./mailer');
+
+describe('Email notification', function(){
+   describe('\nping AWS', function(){
+        it('should return 200', function(done) {
+
+        });
+   });
+});
