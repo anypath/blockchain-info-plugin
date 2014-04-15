4456d7dbe9c19d227aeb5098d1d807b9726d4b19
Abiy Seifu <abiy@ripple.com>
2014-04-15 12:15:17 -0700

    [FEATURE] mailer unit test

--- test/mailer.js
+++ test/mailer.js
@@ -1,10 +1,21 @@
 var config = require('../config/nconf'),
     assert = require('assert'),
-    mailer = require('./mailer');
+    mailer = require('../lib/mailer');
 
 describe('Email notification', function(){
-   describe('\nping AWS', function(){
-        it('should return 200', function(done) {
+   describe('\nsend test email', function(){
+        it('\ntest email should go out, should return sent', function(done) {
+            this.timeout(20000);
+            mailer.sendEmail({
+                to: ['abiy@ripple.com'],
+                subject: 'UNIT TEST EMAIL',
+                body: 'UNIT TEST BODY'
+            }, function(result){
+                assert(result[0].status === 'sent');
+            }, function(e) {
+                console.log('MANDRILL ERROR', e);
+            });
+            done();
 
         });
    });
